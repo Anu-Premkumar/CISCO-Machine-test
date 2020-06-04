e54d00ed23d8eabfc6259f80a3b0fe80322be2e6
Anu Premkumar <51448854+Anu-Premkumar@users.noreply.github.com>
2020-06-04 14:39:20 +0530

    Add files via upload
    Excercise 1: Que 4

---
+++ figures.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(220);
+  ellipse(50,50,80,80);
+  ellipse(100,200,80,80);
+  line(90,50,400,50);
+  ellipse(400,50,80,80);
+  ellipse(350,200,80,80);
+  line(350,160,400,90);
+  line(100,160,50,90);
+  ellipse(220,300,80,80);
+  line(100,240,180,300);
+  line(350,240,260,300);
+}
